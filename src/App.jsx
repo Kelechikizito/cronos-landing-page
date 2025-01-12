e75dc9f2ed13eaee8f4c63e824721f269ecd64e8
Kelechikizito <kaylaychi77@gmail.com>
2025-01-12 14:16:42 +0100

    files update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="bg-[#050505]">
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RiCloseLine } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-[#050505] text-white">
-        <div className="container flex flex-col md:flex-row justify-between">      
+        <div className="flex flex-col md:flex-row justify-between">      
             
             <div className="flex justify-between">
                 <a href="#">
@@ -30,15 +31,15 @@ const Navbar = () => {
             </div>
 
             <div>
-                    <ul className={`text-center md:flex md:items-center md:space-x-6 ${isOpen ? 'block' : 'hidden'} md:block`}>
-                        <li><a href="#learn" className="block py-2 m-5 md:py-0 md:m-0">Learn</a></li>
-                        <li><a href="#build" className="block py-2 m-5 md:py-0 md:m-0">Build</a></li>
-                        <li><a href="#explore" className="block py-2 m-5 md:py-0 md:m-0">Explore</a></li>
-                    </ul>
+                <ul className={`text-center md:flex md:items-center  ${isOpen ? 'block' : 'hidden'} md:block md:gap-10`}>
+                    <li><a href="#learn" className="block py-2 m-5 md:py-0 md:m-0">Learn</a></li>
+                    <li><a href="#build" className="block py-2 m-5 md:py-0 md:m-0">Build</a></li>
+                    <li><a href="#explore" className="block py-2 m-5 md:py-0 md:m-0">Explore</a></li>
+                </ul>
             </div>
 
-            <div className="hidden md:block p-0">
-                <a href="#getTemplate" className="py-2 m-0">Get Template</a>
+            <div className={`text-center p-0 ${isOpen ? 'block' : 'hidden'} md:block`}>
+                <a href="#getTemplate" className="flex gap-2 justify-center">Get CRONOS <span className=" flex justify-center items-center"><IoIosArrowForward className="text-white"/></span></a>
             </div>
 
         </div>
