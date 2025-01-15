08c195e2e55be2a073c5713fb1e3032e8c8c03f0
Kelechikizito <kaylaychi77@gmail.com>
2025-01-15 16:14:16 +0100

    files update

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -14,7 +14,7 @@ const ContactSection = () => {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
-            className="text-3xl sm:text-7xl font-bold bg-gradient-to-r from-white to-gray-500 bg-clip-text text-transparent"
+            className='text-3xl sm:text-7xl font-bold bg-gradient-to-r from-white to-gray-500 bg-clip-text text-transparent'
           >
             Join the network.
           </motion.h1>
